42b6b96369f21cdf5d5c33c5a0e6551cce51f257
Sehar <seharmeharnsj@gmail.com>
2025-05-11 11:08:55 +0500

    Added Substraction Functionality

--- Calculator.py
+++ Calculator.py
@@ -2,3 +2,9 @@ int a=9;
 int b =7;
 int sum = a + b;
 Console.WriteLine("Sum of these numbers are:"+{sum});
+
+//Substraction
+int a=9;
+int b =7;
+int sub = a - b;
+Console.WriteLine("Sub of these numbers are:"+{sub});
